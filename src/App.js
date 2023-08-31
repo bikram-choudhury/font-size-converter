87eca43ee52fee279226ac4ae2056916cdc47772
Bikram Choudhury <bikramchoudhury@Bikrams-MacBook-Air.local>
2023-08-31 20:47:30 +0530

    create ui part 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main className='wrapper flex-center'>
+      <section className='form-group flex-center column'>
+        <div className='input-group'>
+          <input type='text' placeholder='Enter value' />
+          <select>
+            <option value="">Choose...</option>
+            <option value="px">px</option>
+            <option value="em">em</option>
+            <option value="rem">rem</option>
+          </select>
+        </div>
+        <div className='action-btn mt-2'>
+          <button className='btn'>Convert</button>
+        </div>
+      </section>
+    </main>
+  )
 }
 
 export default App;
