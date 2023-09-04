d3f6f217babe1c26bf2d0e99fbb4e51a192bc2bd
bikram-choudhury <bikram.choudhury527@gmail.com>
2023-09-04 17:12:49 +0530

    creating common elements component part 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React from 'react';
 import './App.scss';
+import Input from './common/Elements/Input';
+import Button from './common/Elements/Button';
 
-function App() {
-  return (
-    <main className='wrapper flex-center'>
-      <section className='form-group flex-center column'>
-        <div className='input-group'>
-          <input type='text' placeholder='Enter value' />
-          <select>
-            <option value="">Choose...</option>
-            <option value="px">px</option>
-            <option value="em">em</option>
-            <option value="rem">rem</option>
-          </select>
-        </div>
-        <div className='action-btn mt-2'>
-          <button className='btn'>Convert</button>
-        </div>
-      </section>
-    </main>
-  )
+class App extends React.Component {
+  render() {
+    return (
+      <main className='wrapper flex-center'>
+        <section className='form-group flex-center column'>
+          <div className='input-group'>
+            <Input />
+            <select>
+              <option value="">Choose...</option>
+              <option value="px">px</option>
+              <option value="em">em</option>
+              <option value="rem">rem</option>
+            </select>
+          </div>
+          <div className='action-btn mt-2'>
+            <Button />
+          </div>
+        </section>
+      </main>
+    )
+  }
 }
 
 export default App;
